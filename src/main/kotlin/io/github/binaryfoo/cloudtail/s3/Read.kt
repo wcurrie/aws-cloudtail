457132892a35c4a6bc69a47a4a5da8b53d8e4507
Will Currie <will@currie.id.au>
2017-03-25 12:19:06 +1100

    Closer to a web sequence diagram

--- src/main/kotlin/io/github/binaryfoo/cloudtail/s3/Read.kt
+++ src/main/kotlin/io/github/binaryfoo/cloudtail/s3/Read.kt
@@ -5,6 +5,7 @@ import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailLog
 import com.amazonaws.services.cloudtrail.processinglibrary.model.LogDeliveryInfo
 import com.amazonaws.services.cloudtrail.processinglibrary.serializer.RawLogDeliveryEventSerializer
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.binaryfoo.cloudtail.propertiesFrom
 import java.io.File
 import java.time.LocalDateTime
 import java.time.ZoneId
@@ -14,18 +15,36 @@ import java.util.zip.GZIPInputStream
 private val TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm:ss")
 
 fun main(args: Array<String>) {
+    val exclude = Regex(propertiesFrom("config.properties").getProperty("exclusion_regex"))
     File("tmp/all.wsd").printWriter().use { out ->
         processEvents("tmp") { event ->
+            val eventName = event.eventData.eventName
             val server = event.eventData.eventSource
             val client = event.eventData.sourceIPAddress
             val time = LocalDateTime.ofInstant(event.eventData.eventTime.toInstant(), ZoneId.of("UTC"))
             val userName = event.eventData.userIdentity.userName
             val principalId = event.eventData.userIdentity.principalId
-            val eventMetadata = event.eventMetadata as LogDeliveryInfo
+            val request = compress(event.eventData.requestParameters)
+            val response = compress(event.eventData.responseElements)
+            val rawEvent = (event.eventMetadata as LogDeliveryInfo).rawEvent
 
-            out.println("${TIME_FORMAT.format(time)}: $client -> $server ($userName/$principalId)")
+            if (!exclude.containsMatchIn(rawEvent)) {
+                val optionalUser = userName?.let { " ($it)" }?:""
+                out.println("$client -> $server: ${TIME_FORMAT.format(time)} $eventName$optionalUser $request")
+                if (response != "") {
+                    out.println("$client <-- $server: $response")
+                }
+            }
+
         }
     }
+}
+
+val SessionToken = Regex("\"sessionToken\":\"[^\"]+\"")
+
+fun compress(requestParameters: String?): String {
+    val request = requestParameters?:""
+    return request.replace(SessionToken, "\"sessionToken\":\"<token>\"")
 }
 
 private val mapper = ObjectMapper()

56269846db734a320460c72a03d5ad1f3074869f
Will Currie <will@currie.id.au>
2017-03-25 11:37:14 +1100

    Sort events from each log file

--- src/main/kotlin/io/github/binaryfoo/cloudtail/s3/Read.kt
+++ src/main/kotlin/io/github/binaryfoo/cloudtail/s3/Read.kt
@@ -1,7 +1,9 @@
 package io.github.binaryfoo.cloudtail.s3
 
+import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailEvent
 import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailLog
-import com.amazonaws.services.cloudtrail.processinglibrary.serializer.DefaultEventSerializer
+import com.amazonaws.services.cloudtrail.processinglibrary.model.LogDeliveryInfo
+import com.amazonaws.services.cloudtrail.processinglibrary.serializer.RawLogDeliveryEventSerializer
 import com.fasterxml.jackson.databind.ObjectMapper
 import java.io.File
 import java.time.LocalDateTime
@@ -13,17 +15,29 @@ private val TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm:ss")
 
 fun main(args: Array<String>) {
     val mapper = ObjectMapper()
-    File("tmp").listFiles().sorted().forEach { logFile ->
-        GZIPInputStream(logFile.inputStream()).use { inputStream ->
-            val jsonParser = mapper.factory.createParser(inputStream)
-            val serializer = DefaultEventSerializer(CloudTrailLog("", ""), jsonParser)
-            while (serializer.hasNextEvent()) {
-                val event = serializer.nextEvent
+    File("tmp/all.wsd").printWriter().use { out ->
+        File("tmp").listFiles().filter { it.name.endsWith(".gz") }.sorted().forEach { logFile ->
+            println(logFile.name)
+            // events within a file are not ordered
+            val events = ArrayList<CloudTrailEvent>()
+            GZIPInputStream(logFile.inputStream()).use { inputStream ->
+                val fullLogText = inputStream.reader().readText()
+                val jsonParser = mapper.factory.createParser(fullLogText)
+                val serializer = RawLogDeliveryEventSerializer(fullLogText, CloudTrailLog("", ""), jsonParser)
+                while (serializer.hasNextEvent()) {
+                    events.add(serializer.nextEvent)
+                }
+            }
+            events.sortBy { it.eventData.eventTime }
+            events.forEach { event ->
                 val server = event.eventData.eventSource
                 val client = event.eventData.sourceIPAddress
                 val time = LocalDateTime.ofInstant(event.eventData.eventTime.toInstant(), ZoneId.of("UTC"))
+                val userName = event.eventData.userIdentity.userName
+                val principalId = event.eventData.userIdentity.principalId
+                val eventMetadata = event.eventMetadata as LogDeliveryInfo
 
-                println("${TIME_FORMAT.format(time)}: $client -> $server")
+                out.println("${TIME_FORMAT.format(time)}: $client -> $server ($userName/$principalId)")
             }
         }
     }

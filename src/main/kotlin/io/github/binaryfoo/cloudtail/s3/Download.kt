01f55d2c5637983d4c58894d1cc07729b539d578
Will Currie <will@currie.id.au>
2017-05-15 19:41:52 +1000

    Use nextMarker to download all files

--- src/main/kotlin/io/github/binaryfoo/cloudtail/s3/Download.kt
+++ src/main/kotlin/io/github/binaryfoo/cloudtail/s3/Download.kt
@@ -2,6 +2,8 @@ package io.github.binaryfoo.cloudtail.s3
 
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.GetObjectRequest
+import com.amazonaws.services.s3.model.ListObjectsRequest
+import com.amazonaws.services.s3.model.S3ObjectSummary
 import io.github.binaryfoo.cloudtail.propertiesFrom
 import java.io.File
 import java.util.concurrent.Callable
@@ -11,23 +13,43 @@ import java.util.concurrent.TimeUnit
 import java.util.stream.Collectors
 
 fun main(args: Array<String>) {
-    val properties = propertiesFrom("config.properties")
     val s3Client = AmazonS3Client()
+    val properties = propertiesFrom("config.properties")
     val bucketName = properties.getProperty("bucket_name")
-    val response = s3Client.listObjects(bucketName, properties.getProperty("key_prefix"))
+    val prefix = properties.getProperty("key_prefix")
+    val allFiles = findLogFiles(s3Client, bucketName, prefix)
+
+    download(s3Client, allFiles)
+}
+
+private fun download(s3Client: AmazonS3Client, allFiles: MutableList<S3ObjectSummary>) {
     val pool = ThreadPoolExecutor(5, 10, 5, TimeUnit.SECONDS, LinkedBlockingQueue())
-    val futures = response.objectSummaries.parallelStream().map {
+    val futures = allFiles.parallelStream().map {
         println(it.key)
         val name = File(it.key).name
         pool.submit(Callable<String> {
-            s3Client.getObject(GetObjectRequest(bucketName, it.key), File("tmp", name))
+            s3Client.getObject(GetObjectRequest(it.bucketName, it.key), File("tmp", name))
             name
         })
     }.collect(Collectors.toList())
     futures.forEach {
         println("Downloaded " + it.get())
     }
-    val totalBytes = response.objectSummaries.map { it.size }.sum()
-    println("size $totalBytes")
     pool.shutdown()
+}
+
+private fun findLogFiles(s3Client: AmazonS3Client, bucketName: String?, prefix: String?): MutableList<S3ObjectSummary> {
+    val request = ListObjectsRequest().apply {
+        this.bucketName = bucketName
+        this.prefix = prefix
+    }
+    val allFiles = mutableListOf<S3ObjectSummary>()
+    do {
+        val response = s3Client.listObjects(request)
+        val files = response.objectSummaries.filter { !it.key.contains("-Digest") }
+        allFiles.addAll(files)
+        println("Found ${files.size} files. Next marker ${response.nextMarker}")
+        request.marker = response.nextMarker
+    } while (response.nextMarker != null)
+    return allFiles
 }
